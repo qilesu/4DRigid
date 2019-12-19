0046b33aa4c29b0f0dd0059e5312e4e1a46cc035
David Su <qilesu@g.ucla.edu>
2019-12-18 19:16:46 -0800

    used lyap module in rotation.py

--- src/rotation.py
+++ src/rotation.py
@@ -1,5 +1,6 @@
 import numpy as np
-import quaternion as quat
+import quat
+import lyap
 from numpy.linalg import inv
 
 def getKE(ql,qr,L,J0):
@@ -11,19 +12,7 @@ def solveOmega(ql,qr,L,J0):
     # by expanding out the equation into the form l=A*w
     R = quat.get4Dmatrix(ql,qr)
     J = R.dot(J0).dot(R.T)
-    # A would be much easier if J is diagonal
-    A=np.array([[-J[0,0]-J[1,1],-J[1,2],-J[1,3],J[0,2],J[0,3],0],
-                [-J[1,2],-J[0,0]-J[2,2],-J[2,3],-J[0,1],0,J[0,3]],
-                [-J[1,3],-J[2,3],-J[0,0]-J[3,3],0,-J[0,1],-J[0,2]],
-                [J[0,2],-J[0,1],0,-J[1,1]-J[2,2],-J[2,3],J[1,3]],
-                [J[0,3],0,-J[0,1],-J[2,3],-J[1,1]-J[3,3],-J[1,2]],
-                [0,J[0,3],-J[0,2],J[1,3],-J[1,2],-J[2,2]-J[3,3]]])
-    l=np.array([L[0,1],L[0,2],L[0,3],L[1,2],L[1,3],L[2,3]])
-    w=inv(A).dot(l) # this can be stored so we don't need to recompute it
-    return np.array([[0,w[0],w[1],w[2]],
-                     [-w[0],0,w[3],w[4]],
-                     [-w[1],-w[3],0,w[5]],
-                     [-w[2],-w[4],-w[5],0]])
+    return lyap.solve(L,J)
 
 def step(ql,qr,L,J0,dt):
     Omega=solveOmega(ql,qr,L,J0)
